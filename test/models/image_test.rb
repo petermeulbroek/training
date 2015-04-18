cf43616e322c521ad3d5aa8311432e84d07fc13c
Peter Meulbroek <pmeulbroek@gmail.com>
2015-04-18 16:47:01 -0400

    added image validations

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,6 +1,32 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
+  setup do
+    @image = images(:one)
+  end
+
+  test "fixture is valid" do
+    @image.save
+  end
+  
+  test "requires url" do
+    i = Image.new(reviewed: false)
+    assert_not i.save
+  end
+  
+  test "url must be unique" do
+    @image.save
+    i = Image.new(url: @image.url, reviewed: false)
+    assert_not i.save
+  end
+  
+  test "requires reviewed" do
+    i = Image.new(url: "test")
+    assert_not i.save
+
+  end
+
+
   # test "the truth" do
   #   assert true
   # end
