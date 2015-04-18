71c147f3a8705409aea85d2ec69151d547e3784f
Peter Meulbroek <pmeulbroek@gmail.com>
2015-04-18 16:53:47 -0400

    type model tests

--- test/models/type_test.rb
+++ test/models/type_test.rb
@@ -1,6 +1,22 @@
 require 'test_helper'
 
 class TypeTest < ActiveSupport::TestCase
+  setup do
+    @type = types(:one)
+  end
+
+  test "fixture works" do
+    assert @type.save
+  end
+
+  test "requires type to be present" do
+    t = Type.new()
+    assert_not t.save
+
+    t = Type.new(name: "a tag")
+    assert t.save
+  end
+
   # test "the truth" do
   #   assert true
   # end
