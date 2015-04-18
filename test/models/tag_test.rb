a9a00e20458a82d3fe0134ac5faac9acaa8188bc
Peter Meulbroek <pmeulbroek@gmail.com>
2015-04-18 16:56:01 -0400

    tag model tests

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,6 +1,29 @@
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
+  setup do
+    @tag = tags(:one)
+  end
+
+  test "fixture works" do
+    assert @tag.save
+  end
+
+  test "requires tag to be present" do
+    t = Tag.new()
+    assert_not t.save
+
+    t = Tag.new(value: "a tag")
+    assert t.save
+  end
+
+  test "tag belongs to a type" do
+    type = types(:one)
+    t = Tag.new(value: "a tag")
+    t.type = type
+    assert t.save
+    
+  end
   # test "the truth" do
   #   assert true
   # end
